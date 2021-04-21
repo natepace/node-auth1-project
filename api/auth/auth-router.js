6c096bf70157e04e2c2ed73d7f2bde295454d18a
natepace <n8wpace@gmail.com>
2021-04-21 17:31:12 -0600

    finished

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,20 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
+const router = require('express').Router()
+const mw = require('./auth-middleware.js')
+const User = require('../users/users-model.js')
+const bcrypt = require('bcryptjs')
 
+
+router.post('/register', mw.checkPasswordLength, mw.checkUsernameFree, (req, res, next) => {
+  const { username, password } = req.body
+  const hash = bcrypt.hashSync(password, 8)
+  User.add({ username: username, password: hash })
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(next)
+})
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
@@ -24,7 +38,15 @@
     "message": "Password must be longer than 3 chars"
   }
  */
-
+router.post('/login', mw.checkUsernameExists, mw.checkPasswordLength, async (req, res, next) => {
+  const { username, password } = req.body
+  const [user] = await User.findBy({ username })
+  if (user && bcrypt.compareSync(password, user.password)) {
+    req.session.user = user
+    res.status(200).json({ message: `welcome back ${username}` })
+  }
+  else { next({ status: 401, message: "invalid username or password" }) }
+})
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -42,6 +64,16 @@
   }
  */
 
+router.get('/logout', (req, res) => {
+  if (req.session && req.session.user) {
+    req.session.destroy(() => {
+      res.status(200).json({ message: "you have been logged out" })
+    })
+  }
+  else {
+    res.status(401).json({ message: 'no session found' })
+  }
+})
 
 /**
   3 [GET] /api/auth/logout
@@ -59,5 +91,7 @@
   }
  */
 
- 
+
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+
+module.exports = router
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,29 +1,52 @@
+
+const db = require('../../data/db-config.js')
 /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 function find() {
-
+  return db('users').select('user_id', 'username')
+  // .orderBy('id')
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
 function findBy(filter) {
-
+  return db('users').where(filter)
+  // .orderBy('id')
 }
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-
+  // return db('users').where({ user_id }).first()
+  return db("users")
+    .select("user_id", "username")
+    .where({ user_id })
+    .then(data => {
+      return data[0]
+    })
 }
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
-function add(user) {
-
+async function add(user) {
+  // const [id] = await db('users').insert(user, 'id')
+  // return findById(id)
+  return db("users")
+    .insert(user)
+    .then(ids => {
+      return findById(ids[0])
+    })
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  find,
+  findBy,
+  findById,
+  add
+
+}
